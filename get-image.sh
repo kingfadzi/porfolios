8177b74ffcc4fc85bba4ed29bc210da1e98d74e8
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-10-30 16:16:58 -0400

    init commit

--- get-image.sh
+++ get-image.sh
@@ -8,45 +8,20 @@ if [ -z "$1" ]; then
 fi
 
 IMAGE_TAG="$1"
+SOURCE="${BASE_URL}${IMAGE_TAG}"
+DESTINATION="docker-daemon:${IMAGE_TAG}"
 
-IMAGE_NAME=$(echo "$IMAGE_TAG" | cut -d':' -f1)
-TAG=$(echo "$IMAGE_TAG" | cut -d':' -f2)
-
-SANITIZED_TAG="${IMAGE_TAG//[:\/]/-}"
-DEST_FILENAME="${SANITIZED_TAG}.tar"
-DESTINATION="docker-archive:${DEST_FILENAME}"
-
-if [ -f "${DEST_FILENAME}" ]; then
-  echo "Removing existing file: ${DEST_FILENAME}"
-  rm -f "${DEST_FILENAME}"
-fi
-
-echo "Copying ${IMAGE_TAG} from registry..."
+echo "Copying ${IMAGE_TAG} from registry into Docker..."
 docker run --rm \
   -v "$HOME/.docker:/root/.docker:ro" \
-  -v "$(pwd):/workdir" \
-  -w /workdir \
-  skopeo:latest copy "${BASE_URL}${IMAGE_TAG}" "${DESTINATION}"
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  skopeo:latest copy --src-tls-verify=false "${SOURCE}" "${DESTINATION}"
 
 if [ $? -ne 0 ]; then
   echo "Error: skopeo copy failed."
   exit 1
 fi
 
-echo "Copied to ${DEST_FILENAME}"
-
-echo "Loading ${IMAGE_TAG} into Docker..."
-docker load -i "${DEST_FILENAME}"
-
-if [ $? -ne 0 ]; then
-  echo "Error: docker load failed."
-  exit 1
-fi
-
-echo "Loaded ${IMAGE_TAG} into Docker."
-
-# echo "Removing ${DEST_FILENAME}..."
-# rm -f "${DEST_FILENAME}"
-# echo "Removed ${DEST_FILENAME}"
+echo "Successfully loaded ${IMAGE_TAG} into Docker."
 
 exit 0
