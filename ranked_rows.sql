35c4a0169b0423d94a42ae8a3f7c45c71f17a418
kingfadzi <kingfadzi@gmail.com>
2024-12-07 18:03:59 -0500

    Update ranked_rows.sql

--- ranked_rows.sql
+++ ranked_rows.sql
@@ -1,8 +1,26 @@
-WITH RankedRows AS (
-    SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY lob ORDER BY commit_count DESC) AS rn
-    FROM table
+WITH RankedProjects AS (
+    SELECT
+        pl.language,
+        pm.input_project_id,
+        pm.project_id,
+        pm.number_of_commits,
+        pm.last_commit_date,
+        RANK() OVER (PARTITION BY pl.language ORDER BY pm.number_of_commits DESC, pm.last_commit_date DESC) AS rank
+    FROM
+        project_metrics pm
+    INNER JOIN
+        programming_languages pl ON pm.project_id = pl.project_id
 )
-SELECT *
-FROM RankedRows
-WHERE rn = 1;
+SELECT
+    language,
+    input_project_id,
+    project_id,
+    number_of_commits,
+    last_commit_date
+FROM
+    RankedProjects
+WHERE
+    rank <= 50
+ORDER BY
+    language,
+    rank;
