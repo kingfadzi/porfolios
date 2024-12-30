c1811a28ce4ad1bb67e13d894927d5467b1848f4
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-12-30 11:06:49 -0500

    compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 set -e
 
-# Function to start PostgreSQL
+# Start PostgreSQL function
 start_postgres() {
     exec postgres -D "$PGDATA"
 }
 
-# Check if PGDATA is empty
+# If the data directory is empty, run initdb
 if [ -z "$(ls -A "$PGDATA")" ]; then
     echo "Initializing PostgreSQL database..."
     initdb -D "$PGDATA"
 
-    # Modify postgresql.conf to listen on all addresses
+    # Sample config changes (listens on all, allows all inbound)
     echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
-
-    # Modify pg_hba.conf to allow connections from all hosts
     echo "host all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 
-    # Start PostgreSQL temporarily to set up initial database and user
+    # Start Postgres in background to create initial user/database
     pg_ctl -D "$PGDATA" -o "-c listen_addresses='localhost'" -w start
 
-    if [ "$POSTGRES_USER" ] && [ "$POSTGRES_PASSWORD" ]; then
+    # If you set POSTGRES_USER/POSTGRES_PASSWORD in `docker run -e`, create that user
+    if [ -n "$POSTGRES_USER" ] && [ -n "$POSTGRES_PASSWORD" ]; then
         echo "Creating user $POSTGRES_USER..."
         psql --username postgres <<-EOSQL
             CREATE USER "$POSTGRES_USER" WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';
 EOSQL
     fi
 
-    if [ "$POSTGRES_DB" ]; then
+    # If you set POSTGRES_DB in `docker run -e`, create that DB
+    if [ -n "$POSTGRES_DB" ]; then
         echo "Creating database $POSTGRES_DB..."
         psql --username postgres <<-EOSQL
             CREATE DATABASE "$POSTGRES_DB" WITH OWNER "$POSTGRES_USER";
 EOSQL
     fi
 
+    # Stop the temporary Postgres
     pg_ctl -D "$PGDATA" -m fast -w stop
 fi
 
-# Start PostgreSQL
+# Finally, start Postgres (foreground)
 start_postgres
