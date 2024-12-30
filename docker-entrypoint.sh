0fcfd4bcc80cf4131e451dc1a9e4884cd97e1e7a
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-12-30 11:29:51 -0500

    compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 set -e
 
-# If the database is not initialized (i.e., PG_VERSION is missing),
-# then run initdb and configure the basics.
 if [ ! -f "$PGDATA/PG_VERSION" ]; then
     echo "Initializing PostgreSQL database..."
     initdb -D "$PGDATA"
 
-    # Sample configuration changes
-    echo "listen_addresses = '*'" >> "$PGDATA/postgresql.conf"
-    echo "host all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+    # Now create the log folder after initdb is done
+    mkdir -p "$PGDATA/log"
 
-    # You can optionally start a temporary Postgres to create
-    # users/databases:
-    # pg_ctl -D "$PGDATA" -o "-c listen_addresses='localhost'" -w start
-    #
-    # psql --username postgres <<-EOSQL
-    #    CREATE USER myuser WITH SUPERUSER PASSWORD 'mypassword';
-    #    CREATE DATABASE mydb WITH OWNER myuser;
-    # EOSQL
-    #
-    # pg_ctl -D "$PGDATA" -m fast -w stop
+    echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
+    echo "host all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 fi
 
-# Exec the final command (usually "postgres -D /var/lib/pgsql/data")
 exec "$@"
