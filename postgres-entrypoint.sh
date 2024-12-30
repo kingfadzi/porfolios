ecbfb4da6776af74bc28f5be7c0c82ed6d22f49d
Fadzi Ushewokunze <kingfadzi@gmail.com>
2024-12-29 23:39:13 -0500

    compose

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -9,16 +9,16 @@ chmod 700 /var/lib/pgsql/data
 # Initialize PostgreSQL if not already initialized
 if [ ! -f /var/lib/pgsql/data/postgresql.conf ]; then
     echo "Initializing PostgreSQL database..."
-    runuser -u postgres -- initdb -D /var/lib/pgsql/data
+    initdb -D /var/lib/pgsql/data
 
     # Configure PostgreSQL for external connections
-    echo "host all all 0.0.0.0/0 md5" >> /var/lib/pgsql/data/pg_hba.conf
     echo "listen_addresses = '*'" >> /var/lib/pgsql/data/postgresql.conf
+    echo "host all all 0.0.0.0/0 md5" >> /var/lib/pgsql/data/pg_hba.conf
     echo "PostgreSQL database initialized."
 else
     echo "Database already initialized."
 fi
 
-# Start PostgreSQL as the postgres user
+# Start PostgreSQL
 echo "Starting PostgreSQL..."
-exec runuser -u postgres -- postgres -D /var/lib/pgsql/data
+exec postgres -D /var/lib/pgsql/data
