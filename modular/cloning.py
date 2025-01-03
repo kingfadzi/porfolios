db1051032be1b9ca669ea55a0bd71122c15813ef
kingfadzi <kingfadzi@gmail.com>
2025-01-03 17:28:41 -0500

    update cloning.py

--- modular/cloning.py
+++ modular/cloning.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import threading
+import re
 from modular.models import Session, Repository
 from modular.execution_decorator import analyze_execution
 from modular.config import Config
