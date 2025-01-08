792c62b8229d6d01d324b282dba28e95c762b530
kingfadzi <kingfadzi@gmail.com>
2025-01-07 20:21:14 -0500

    update uber_table.sql

--- sql/uber_table.sql
+++ sql/uber_table.sql
@@ -1,83 +1,148 @@
-CREATE MATERIALIZED VIEW uber_dataset_mv AS
+CREATE OR REPLACE VIEW combined_repo_metrics AS
+WITH
+
+-- 1) Enumerate all repos from across the various tables.
+all_repos AS (
+    SELECT repo_id FROM lizard_summary
+    UNION
+    SELECT repo_id FROM cloc_metrics
+    UNION
+    SELECT repo_id FROM grype_results
+    UNION
+    SELECT repo_id FROM checkov_summary
+    UNION
+    SELECT repo_id FROM trivy_vulnerability
+    UNION
+    SELECT repo_id FROM semgrep_results
+    UNION
+    SELECT repo_id FROM repo_metrics    -- Include repos found in repo_metrics
+),
+
+-- 2) Example aggregations for cloc_metrics, grype, etc.
+cloc_agg AS (
+    SELECT
+        repo_id,
+        SUM(files)   AS total_files,
+        SUM(blank)   AS total_blank,
+        SUM(comment) AS total_comment,
+        SUM(code)    AS total_code
+    FROM cloc_metrics
+    GROUP BY repo_id
+),
+grype_agg AS (
+    SELECT
+        repo_id,
+        COUNT(*) AS total_grype_vulns,
+        COUNT(*) FILTER (WHERE severity = 'Critical') AS critical_vulns,
+        COUNT(*) FILTER (WHERE severity = 'High')     AS high_vulns,
+        COUNT(*) FILTER (WHERE severity = 'Medium')   AS medium_vulns,
+        COUNT(*) FILTER (WHERE severity = 'Low')      AS low_vulns
+    FROM grype_results
+    GROUP BY repo_id
+),
+checkov_agg AS (
+    SELECT
+        repo_id,
+        SUM(passed)  AS total_passed,
+        SUM(failed)  AS total_failed,
+        SUM(skipped) AS total_skipped,
+        SUM(resource_count) AS total_resources,
+        SUM(parsing_errors) AS total_parsing_errors
+    FROM checkov_summary
+    GROUP BY repo_id
+),
+trivy_agg AS (
+    SELECT
+        repo_id,
+        COUNT(*) AS total_trivy_vulns,
+        COUNT(*) FILTER (WHERE severity = 'CRITICAL') AS trivy_critical,
+        COUNT(*) FILTER (WHERE severity = 'HIGH')     AS trivy_high,
+        COUNT(*) FILTER (WHERE severity = 'MEDIUM')   AS trivy_medium,
+        COUNT(*) FILTER (WHERE severity = 'LOW')      AS trivy_low
+    FROM trivy_vulnerability
+    GROUP BY repo_id
+),
+semgrep_agg AS (
+    SELECT
+        repo_id,
+        COUNT(*) AS total_semgrep_findings,
+        COUNT(*) FILTER (WHERE severity = 'ERROR')   AS semgrep_error,
+        COUNT(*) FILTER (WHERE severity = 'WARNING') AS semgrep_warning,
+        COUNT(*) FILTER (WHERE severity = 'INFO')    AS semgrep_info
+    FROM semgrep_results
+    GROUP BY repo_id
+)
+
+-- 3) Final SELECT: 
+--    Join all aggregated sub-results + the lizard_summary + repo_metrics
 SELECT
     r.repo_id,
-    r.repo_name,
-    r.repo_slug,
-    r.status AS repo_status,
-    rm.activity_status,
-    r.updated_on AS last_updated,
-    gm.language AS primary_language,
-    gm.percent_usage AS language_usage_percent,
+
+    -- lizard_summary is already 1:1 with repo_id
+    l.total_nloc,
+    l.avg_ccn,
+    l.total_token_count,
+    l.function_count,
+    l.total_ccn     AS total_ccn_lizard,
+
+    -- From cloc_agg
+    c.total_files,
+    c.total_blank,
+    c.total_comment,
+    c.total_code,
+
+    -- From grype_agg
+    g.total_grype_vulns,
+    g.critical_vulns,
+    g.high_vulns,
+    g.medium_vulns,
+    g.low_vulns,
+
+    -- From checkov_agg
+    ck.total_passed,
+    ck.total_failed,
+    ck.total_skipped,
+    ck.total_resources,
+    ck.total_parsing_errors,
+
+    -- From trivy_agg
+    t.total_trivy_vulns,
+    t.trivy_critical,
+    t.trivy_high,
+    t.trivy_medium,
+    t.trivy_low,
+
+    -- From semgrep_agg
+    s.total_semgrep_findings,
+    s.semgrep_error,
+    s.semgrep_warning,
+    s.semgrep_info,
+
+    -- Directly from repo_metrics table (no aggregation needed)
     rm.repo_size_bytes,
     rm.file_count,
     rm.total_commits,
     rm.number_of_contributors,
+    rm.activity_status,
     rm.last_commit_date,
     rm.repo_age_days,
     rm.active_branch_count,
-    ls.total_nloc AS total_lines_of_code,
-    ls.avg_ccn AS avg_complexity_score,
-    ls.total_ccn AS total_complexity_score,
-    ls.function_count AS total_functions,
-    cm.language AS cloc_language,
-    cm.code AS cloc_code_lines,
-    cm.comment AS cloc_comment_lines,
-    cm.blank AS cloc_blank_lines,
-    gr.id AS grype_id,
-    gr.cve AS grype_cve,
-    gr.severity AS grype_severity,
-    gr.package AS grype_package,
-    gr.version AS grype_version,
-    gr.language AS grype_language,
-    cs.id AS checkov_id,
-    cs.check_type AS check_type,
-    cs.passed AS passed,
-    cs.failed AS failed,
-    cs.skipped AS skipped,
-    cs.resource_count AS resource_count,
-    tv.id AS trivy_id,
-    tv.target AS trivy_target,
-    tv.resource_class AS trivy_resource_class,
-    tv.resource_type AS trivy_resource_type,
-    tv.vulnerability_id AS trivy_vulnerability_id,
-    tv.pkg_name AS trivy_pkg_name,
-    tv.installed_version AS trivy_installed_version,
-    tv.fixed_version AS trivy_fixed_version,
-    tv.severity AS trivy_severity,
-    sr.id AS semgrep_id,
-    sr.path AS semgrep_path,
-    sr.start_line AS semgrep_start_line,
-    sr.end_line AS semgrep_end_line,
-    sr.rule_id AS semgrep_rule_id,
-    sr.severity AS semgrep_severity,
-    sr.message AS semgrep_message,
-    sr.category AS semgrep_category
-FROM
-    repository r
-        LEFT JOIN
-    go_enry_analysis gm ON r.repo_id = gm.repo_id
-        LEFT JOIN
-    repo_metrics rm ON r.repo_id = rm.repo_id
-        LEFT JOIN
-    lizard_summary ls ON r.repo_id = ls.repo_id
-        LEFT JOIN
-    cloc_metrics cm ON r.repo_id = cm.repo_id
-        LEFT JOIN
-    grype_results gr ON r.repo_id = gr.repo_id
-        LEFT JOIN
-    checkov_summary cs ON r.repo_id = cs.repo_id
-        LEFT JOIN
-    trivy_vulnerability tv ON r.repo_id = tv.repo_id
-        LEFT JOIN
-    semgrep_results sr ON r.repo_id = sr.repo_id;
+    rm.updated_at
 
+FROM all_repos r
+-- Join lizard_summary
+LEFT JOIN lizard_summary       l  ON r.repo_id = l.repo_id
+-- Join cloc_agg
+LEFT JOIN cloc_agg            c  ON r.repo_id = c.repo_id
+-- Join grype_agg
+LEFT JOIN grype_agg           g  ON r.repo_id = g.repo_id
+-- Join checkov_agg
+LEFT JOIN checkov_agg         ck ON r.repo_id = ck.repo_id
+-- Join trivy_agg
+LEFT JOIN trivy_agg           t  ON r.repo_id = t.repo_id
+-- Join semgrep_agg
+LEFT JOIN semgrep_agg         s  ON r.repo_id = s.repo_id
+-- Finally, join repo_metrics (1 row per repo)
+LEFT JOIN repo_metrics        rm ON r.repo_id = rm.repo_id
 
-CREATE INDEX idx_repository_repo_id ON repository(repo_id);
-CREATE INDEX idx_go_enry_analysis_repo_id ON go_enry_analysis(repo_id);
-CREATE INDEX idx_repo_metrics_repo_id ON repo_metrics(repo_id);
-CREATE INDEX idx_lizard_summary_repo_id ON lizard_summary(repo_id);
-CREATE INDEX idx_cloc_metrics_repo_id ON cloc_metrics(repo_id);
-CREATE INDEX idx_grype_results_repo_id ON grype_results(repo_id);
-CREATE INDEX idx_checkov_summary_repo_id ON checkov_summary(repo_id);
-CREATE INDEX idx_trivy_vulnerability_repo_id ON trivy_vulnerability(repo_id);
-CREATE INDEX idx_semgrep_results_repo_id ON semgrep_results(repo_id);
+ORDER BY r.repo_id;
