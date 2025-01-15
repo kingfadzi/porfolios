7d0c37ea02885f3e9ae0a3394bf93b85d959c288
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-01-15 17:24:14 -0500

    caching

--- sql/erd.sql
+++ sql/erd.sql
@@ -1,20 +1,14 @@
 SELECT
-    SCHEMA_NAME(o.schema_id) AS [SchemaName],
-    o.name AS [ViewName],
-    c.column_id AS [ColumnID],
-    c.name AS [ColumnName],
-    t.name AS [DataType],
-    c.max_length AS [MaxLength],
+    t.name AS TableName,
+    c.name AS ColumnName,
+    ty.name AS DataType,
+    c.max_length AS MaxLength,
     c.precision AS [Precision],
     c.scale AS [Scale],
-    c.is_nullable AS [IsNullable]
-FROM
-    sys.objects AS o
-    INNER JOIN sys.columns AS c ON o.object_id = c.object_id
-    INNER JOIN sys.types AS t ON c.user_type_id = t.user_type_id
-WHERE
-    o.type = 'V'  -- 'V' indicates a View
-ORDER BY
-    [SchemaName],
-    [ViewName],
-    c.column_id;
+    c.is_nullable AS IsNullable
+FROM sys.tables t
+    INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+    INNER JOIN sys.columns c ON t.object_id = c.object_id
+    INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
+WHERE s.name = 'my_schema'
+ORDER BY t.name, c.column_id;
