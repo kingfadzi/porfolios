a3647be7e21b221c27135e85042db58bfbbbd06d
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-01-16 13:38:45 -0500

    caching

--- sql/mapping.sql
+++ sql/mapping.sql
@@ -13,3 +13,24 @@ FROM repo AS r
               ON cm_v.component_id = cm_b.component_id
 WHERE cm_v.mapping_type = 'vs'
   AND cm_b.mapping_type = 'ba';
+
+SELECT
+    LOWER(b.project_key) AS pkey,
+    LOWER(b.repo_slug)   AS slug,
+    cm_v.component_id,
+    COUNT(cm_b.identifier) AS identifier_count,
+    STRING_AGG(cm_b.identifier, ', ' ORDER BY cm_b.identifier) AS all_identifiers
+FROM bitbucket_repositories b
+         LEFT JOIN component_mapping cm_v
+                   ON  LOWER(cm_v.project_key) = LOWER(b.project_key)
+                       AND LOWER(cm_v.repo_slug)   = LOWER(b.repo_slug)
+                       AND cm_v.mapping_type       = 'vs'
+         LEFT JOIN component_mapping cm_b
+                   ON  cm_b.component_id = cm_v.component_id
+                       AND cm_b.mapping_type = 'ba'
+GROUP BY
+    LOWER(b.project_key),
+    LOWER(b.repo_slug),
+    cm_v.component_id
+HAVING
+    COUNT(cm_b.identifier) > 1;
